a98920c291f4f23be9d644d5e6b95655ba4e2146
sunsingh <sunsingh@adobe.com>
2017-01-26 15:17:18 +0530

    Login fucntionality

---
+++ src/com/ziletech/logistic/login/Login.java
@@ -0,0 +1,15 @@
+package com.ziletech.logistic.login;
+
+/**
+ *
+ * @author Sunil
+ */
+public class Login {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Trying to login here");
+        }
+    }
+
+}
