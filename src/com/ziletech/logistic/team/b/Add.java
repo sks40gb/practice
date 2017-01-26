273748fa30fb9df2dc22eb0aa11f44249e0799f2
nagendra <nagendra@ziletech.com>
2017-01-26 17:42:22 +0530

    add.java added and modified TeamB.java

---
+++ src/com/ziletech/logistic/team/b/Add.java
@@ -0,0 +1,15 @@
+package com.ziletech.logistic.team.b;
+
+/**
+ *
+ * @author Nagendra
+ */
+public class Add {
+
+    public static void main(String args[]) {
+
+        int x, y = 4, z = 5;
+        x = y + z;
+        System.out.println("result is" + x);
+    }
+}
