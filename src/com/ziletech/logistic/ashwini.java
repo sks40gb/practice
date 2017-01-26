8627de711e5f8fcec72aa1665f72999403d8a769
Ashwini <ashwini@ziletech.com>
2017-01-26 12:50:40 +0530

    ashwini.java has been created

---
+++ src/com/ziletech/logistic/ashwini.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ziletech.logistic;
+
+/**
+ *
+ * @author user
+ */
+public class ashwini {
+    
+    public static void main(String argu){
+        System.out.println("hello this is main method of thid class");
+    }
+    
+}
