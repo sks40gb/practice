cc5272f7a9de2c6fe89ed950f7b6130474f93559
sunsingh <sunsingh@adobe.com>
2017-01-26 11:12:33 +0530

    Initial Setup

---
+++ src/com/ziletech/logistic/Sunil.java
@@ -0,0 +1,14 @@
+package com.ziletech.logistic;
+
+/**
+ *
+ * @author Sunil
+ */
+public class Sunil {
+
+    public static void main(String[] args) {
+        System.out.println("This is from main method created by sunil");
+
+    }
+
+}
