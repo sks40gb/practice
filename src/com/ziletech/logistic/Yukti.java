120bfcf029bae6b0f1ade901429bad07c1c1e93f
Yukti <yukti@ziletech.com>
2017-01-26 13:18:41 +0530

    Yukti.java

---
+++ src/com/ziletech/logistic/Yukti.java
@@ -0,0 +1,13 @@
+package com.ziletech.logistic;
+
+/**
+ *
+ * @author Yukti
+ */
+public class Yukti {
+    public static void main(String[] args) {
+        System.out.println("This is from main method created by yukti");
+
+    }
+
+}
