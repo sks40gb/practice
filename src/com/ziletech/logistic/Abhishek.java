2ef217d8d2820eb19953ecb5f216c9f70e237ffd
abhishekziletech <abhishek@ziletech.com>
2017-01-26 16:40:16 +0530

    Exsisting File updates and New File added by Abhishek Mane

--- src/com/ziletech/logistic/Abhishek.java
+++ src/com/ziletech/logistic/Abhishek.java
@@ -7,6 +7,7 @@ public class Abhishek {
 
     public static void main(String[] args) {
         System.out.println("HI EVERYONE ");
+        System.out.println("This is Edited Java File");
     }
 
 }
---
+++ src/com/ziletech/logistic/Details.java
@@ -0,0 +1,18 @@
+
+package com.ziletech.logistic;
+
+/**
+ *
+ * @author Abhishek Mane
+ */
+public class Details {
+
+    public static void main(String[] args) {
+
+        System.out.println("Name:Abhishek Mane");
+        System.out.println("Height-5.6");
+        System.out.println("Mob.-9893360190");
+     
+    }
+
+}
